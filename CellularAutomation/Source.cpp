c0943333e40aeeb187338a8c24a9574e0a5cbb72
prostotema1 <artemkorolev-2002@yandex.ru>
2022-05-12 12:35:00 +0300

    Minor changes

--- CellularAutomation/Source.cpp
+++ CellularAutomation/Source.cpp
@@ -18,17 +18,32 @@ void main(){
 	int cell_size;
 	std::cout << "\nSize of a cell = ";
 	std::cin >> cell_size;
+	while (height < 0 || wight < 0 || cell_size < 0) {
+		std::cout << "\nWrong input. Enter correct information\nHeight = ";
+		std::cin >> height;
+		std::cout << "\nWight = ";
+		std::cin >> wight;
+		std::cout << "\nCell size = ";
+		std::cin >> cell_size;
+	}
 	f.init(height, wight, cell_size);
 	std::cout << "\nEnter the number of alive cells\nCells alive = ";
 	int n;
 	std::cin >> n;
-	std::cout << std::endl;
+	while (n < 0) {
+		std::cout << "Wrong data. Enter the right one\nCells alive = ";
+		std::cin >> n;
+	}
 	f.random_fill(n);
-	std::cout << "\nYour generated field\nEnter the number of iterations\nIterations = ";
-	int num;
-	std::cin >> num;
+	std::cout << "\nField has been generated\nEnter the number of generations\nGenerations = ";
+	int generations;
+	std::cin >> generations;
+	while (generations < 0) {
+		std::cout << "\nWrong data. Enter the right one\nGenerations = ";
+		std::cin >> generations;
+	}
 	f.init_graphic();
-	for (int i = 0; i < num; i++){
+	for (int i = 0; i < generations; i++){
 		f.next_iteration();
 		SDL_UpdateWindowSurface(f.window);
 		if (LIMIT_RATE) { SDL_Delay(TICK_RATE); }
